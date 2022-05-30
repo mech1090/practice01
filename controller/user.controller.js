956fa280dcea374284b7599b3ffd8b64b6a156a8
Shahzad <mech1090@gmail.com>
2022-05-30 09:10:49 -0600

    routes and controller initial setup

---
+++ controller/user.controller.js
@@ -0,0 +1,9 @@
+const { model } = require('mongoose')
+const User = require('../model/user')
+
+const getLoginForm = (req,res)=>{}
+const login = (req,res)=>{}
+const getSignupForm = (req,res)=>{}
+const signup = (req,res)=>{}
+
+module.exports ={getLoginForm,login,getSignupForm,signup}
